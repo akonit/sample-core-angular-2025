c6e6674da170a52050720702cb35edee5cf1b0e9
Anna Khaperskaia <hakyfy@gmail.com>
2025-04-02 12:59:55 +0200

    formatting

--- SampleProject/Application/Queries/GetForecasts/GetForecastsQueryHandler.cs
+++ SampleProject/Application/Queries/GetForecasts/GetForecastsQueryHandler.cs
@@ -8,17 +8,17 @@ namespace Application.Queries.GetForecasts
         {
             var summaries = new[]
             {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
 
             var forecast = Enumerable.Range(1, 5).Select(index =>
-            new WeatherForecastDto
-            (
-                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                Random.Shared.Next(-20, 55),
-                summaries[Random.Shared.Next(summaries.Length)]
-            ))
-            .ToArray();
+                new WeatherForecastDto
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+                .ToArray();
 
             return Task.FromResult(forecast);
         }
