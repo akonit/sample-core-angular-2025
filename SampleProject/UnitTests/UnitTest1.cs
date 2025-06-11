1af150ec865539e43f2bcffa8557ac86692cd759
Anna Khaperskaia <hakyfy@gmail.com>
2025-06-11 17:54:24 +0200

    tests

---
+++ SampleProject/UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void CheckTestsAreRunning()
+    {
+        // Arrange
+        var first = 1;
+        var second = 2;
+
+        // Act
+        var result = first + second;
+
+        // Assert
+        Assert.Equal(3, result);
+    }
+}
