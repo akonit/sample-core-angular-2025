663129697a229504ae7f2e93f212fc5c685d8f00
Anna Khaperskaia <hakyfy@gmail.com>
2025-06-11 19:20:07 +0200

    integration tests

---
+++ SampleProject/IntegrationTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Npgsql;
+
+namespace IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void CheckSqlConnection()
+    {
+        var connectionString = "Host=localhost;Port=5432;Username=topSecretUser;Password=topSecretPassword;Database=topSecretDb";
+
+        using var connection = new NpgsqlConnection(connectionString);
+        connection.Open();
+
+        using var command = new NpgsqlCommand("SELECT 1", connection);
+        var result = command.ExecuteScalar();
+
+        Assert.Equal(1, result);
+    }
+}
