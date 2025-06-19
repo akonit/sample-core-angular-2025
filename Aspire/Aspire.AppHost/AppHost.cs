0c114bd2e9dd879bbe4eab3394f93381f7742c25
Anna Khaperskaia <hakyfy@gmail.com>
2025-06-19 17:35:18 +0200

    angular to aspire

--- Aspire/Aspire.AppHost/AppHost.cs
+++ Aspire/Aspire.AppHost/AppHost.cs
@@ -2,6 +2,9 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var weatherService = builder.AddProject<Projects.Api>("weatherservice");
 
-var gateway = builder.AddProject<Projects.GatewayApi>("gateway").WaitFor(weatherService);
+var webUi = builder.AddNpmApp("angular", "../../SampleUi")
+    .WaitFor(weatherService);
+
+var gateway = builder.AddProject<Projects.GatewayApi>("gateway").WaitFor(webUi);
 
 builder.Build().Run();
--- Aspire/README.md
+++ Aspire/README.md
@@ -1,6 +1,6 @@
 Simple way to launch all backend services
 ===
 
-- launch web ui (out of aspire)
 - dotnet run --project Aspire.AppHost
-- https://localhost:17048
+- http://localhost:4200/weather - applicaiton
+- https://localhost:17048 - aspire dashboard
